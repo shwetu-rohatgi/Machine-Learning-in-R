317745f24195840941958d26cf3392c8fc74444c
Shwetanshu Rohatgi <shwetanshu.rohatgi@gmail.com>
2018-03-18 03:08:33 +0530

    multi-regression

--- Part 2 - Regression/Section 5 - Multiple Linear Regression/my_multiple_linear_regression.R
+++ Part 2 - Regression/Section 5 - Multiple Linear Regression/my_multiple_linear_regression.R
@@ -16,3 +16,22 @@ set.seed(123)
 split = sample.split(dataset$Profit, SplitRatio = 4/5) #dependent variable is first argument
 training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
+
+#Multiple linear regressor 
+regressor = lm(formula = Profit ~ .,  #here ~. takes into account all the possible independent variables
+               data = training_set)
+
+#Making predictions on test set
+y_pred = predict(regressor, newdata = test_set)
+
+#Visualizing our predictions 
+library(ggplot2)
+ggplot()+
+  geom_point(aes(x=test_set$R.D.Spend,y=test_set$Profit),
+             colour = "red")+
+  geom_line(aes(x = training_set$R.D.Spend, y = predict(regressor, newdata = training_set)),
+            colour = "green")+
+  xlab('R.D.Spend')+
+  ylab('Profits')+
+  ggtitle('Predicting profits wrt to R.D.Spend')
+  
