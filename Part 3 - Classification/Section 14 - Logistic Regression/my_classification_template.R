cb32318a196678ad7c0ef862c5c68635e3190e06
Shwetanshu Rohatgi <shwetanshu.rohatgi@gmail.com>
2018-04-13 02:12:02 +0530

    K-NN implementation

---
+++ Part 3 - Classification/Section 14 - Logistic Regression/my_classification_template.R
@@ -0,0 +1,64 @@
+#loading working directory
+
+#loading the dataset
+dataset = read.csv('Social_Network_Ads.csv')
+dataset = dataset[,3:5]
+
+#Split dataset
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Purchased, SplitRatio = 0.75)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+#scaling the dataset
+training_set[,1:2] = scale(training_set[,1:2])
+test_set[,1:2] = scale(test_set)[,1:2]
+
+#Logistic regression Model building
+#Create your own classifier based on the problem and accuracy requirements.
+
+#predictions for the test set
+prob_pred = predict(classifier, type = 'response', newdata = test_set)
+#prob_pred
+y_pred = ifelse(prob_pred>.5, 1, 0)
+#y_pred
+
+#Confusion Matrix
+cm = table(test_set[, 3], y_pred) #helps to make a matrix
+cm #this gives the overview of no of correct and incorrect predictions made by our model.
+#We made 57+26=83 correct predictions and 17 were incorrect predictions.
+
+#visualizing the classification results. 
+library(ElemStatLearn)
+set = training_set
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
+grid_set = expand.grid(X1, X2)
+colnames(grid_set) = c('Age', 'EstimatedSalary')
+prob_set = predict(classifier, type = 'response', newdata = grid_set)
+y_grid = ifelse(prob_set > 0.5, 1, 0)
+plot(set[, -3],
+     main = 'Logistic Regression (Training set)',
+     xlab = 'Age', ylab = 'Estimated Salary',
+     xlim = range(X1), ylim = range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
+
+# Visualising the Test set results
+library(ElemStatLearn)
+set = test_set
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
+grid_set = expand.grid(X1, X2)
+colnames(grid_set) = c('Age', 'EstimatedSalary')
+prob_set = predict(classifier, type = 'response', newdata = grid_set)
+y_grid = ifelse(prob_set > 0.5, 1, 0)
+plot(set[, -3],
+     main = 'Logistic Regression (Test set)',
+     xlab = 'Age', ylab = 'Estimated Salary',
+     xlim = range(X1), ylim = range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
